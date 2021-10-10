33b86e24f03559576e6784d5cd8010ffdca2f5c1
Luzzy <luzzyzerio@gmail.com>
2021-10-10 22:59:08 +0800

    添加可交互弹球shader

---
+++ Assets/Scripts/BounceBall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IElastic
+{
+    void OnElastic(RaycastHit hit);
+}
+[RequireComponent(typeof(MeshRenderer))]
+public class BounceBall : MonoBehaviour,IElastic
+{
+    private static int _pos, _normal, _time;
+    private MeshRenderer mesh;
+
+    static BounceBall()
+    {
+
+        _pos = Shader.PropertyToID("_Position");
+
+        _normal = Shader.PropertyToID("_Normal");
+
+        _time = Shader.PropertyToID("_PointTime");
+
+    }
+    private void Awake()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+    public void OnElastic(RaycastHit hit)
+    {
+        Vector4 hitPos = transform.InverseTransformPoint(hit.point);
+        hitPos.w = 0.6f;
+        mesh.material.SetVector(_pos, hitPos);
+
+        Vector4 normalDirect = transform.InverseTransformDirection(hit.normal.normalized);
+        normalDirect.w = 0.2f;
+        mesh.material.SetVector(_normal, normalDirect);
+        mesh.material.SetFloat(_time, Time.time);
+    }
+
+    private void OnMouseDown()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit))
+        {
+            OnElastic(hit);
+        }
+    }
+}
---
+++ Assets/Scripts/main/MainManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public List<GameObject> list;
+
+    private void DetectMouseRaycast()
+    {
+
+    }
+
+}
